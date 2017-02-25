a1d52ce82084cf0dabd8df61727cc1686a4d3dea
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-25 17:25:43 -0500

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * unset-value <https://github.com/jonschlinkert/unset-value>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
